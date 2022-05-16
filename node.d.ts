238e0835f47b0b7b451cc6171c6fc056a1b91d52
nin-jin <nin-jin@users.noreply.github.com>
2022-05-16 07:13:19 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -515,6 +515,7 @@ declare namespace $ {
         hover: $mol_style_func<"var", "--mol_theme_hover">;
         card: $mol_style_func<"var", "--mol_theme_card">;
         current: $mol_style_func<"var", "--mol_theme_current">;
+        special: $mol_style_func<"var", "--mol_theme_special">;
         text: $mol_style_func<"var", "--mol_theme_text">;
         control: $mol_style_func<"var", "--mol_theme_control">;
         shade: $mol_style_func<"var", "--mol_theme_shade">;
@@ -2205,6 +2206,7 @@ declare namespace $ {
         uri(val?: any): string;
         mime(): string;
         title(val?: any): string;
+        Fallback(): $$.$mol_link;
     }
 }
 
