c8cb27e9ee245ac776dcea8a79a410b75192ef16
nin-jin <nin-jin@users.noreply.github.com>
2022-05-20 07:09:48 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2205,7 +2205,8 @@ declare namespace $ {
         uri(val?: any): string;
         mime(): string;
         title(val?: any): string;
-        Fallback(): $$.$mol_link;
+        Fallback_image(): $mol_image;
+        Fallback_link(): $$.$mol_link;
     }
 }
 
--- web.d.ts
+++ web.d.ts
@@ -2185,7 +2185,8 @@ declare namespace $ {
         uri(val?: any): string;
         mime(): string;
         title(val?: any): string;
-        Fallback(): $$.$mol_link;
+        Fallback_image(): $mol_image;
+        Fallback_link(): $$.$mol_link;
     }
 }
 
