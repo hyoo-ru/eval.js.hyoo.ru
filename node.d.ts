3c0dc341029d79cc046de14860dc5e059f915bc1
nin-jin <nin-jin@users.noreply.github.com>
2022-06-06 07:09:14 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -603,6 +603,10 @@ declare namespace $ {
 }
 
 declare namespace $ {
+    function $mol_wire_probe<Value>(task: () => Value, next?: Value): Value | undefined;
+}
+
+declare namespace $ {
     function $mol_wire_watch(): void;
 }
 
@@ -684,7 +688,7 @@ declare namespace $ {
             right: number;
             top: number;
             bottom: number;
-        };
+        } | null;
         dom_id(): any;
         dom_node(next?: Element): Element;
         dom_final(): Element | undefined;
@@ -953,10 +957,6 @@ declare namespace $ {
         Placeholder(): $mol_view;
         pages(): readonly $mol_view[];
     }
-}
-
-declare namespace $ {
-    function $mol_wire_probe<Value>(task: () => Value, next?: Value): Value | undefined;
 }
 
 declare namespace $ {
