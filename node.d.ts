95dcdb779e78dabc98bd9231bd52d9235c0fb422
nin-jin <nin-jin@users.noreply.github.com>
2023-01-24 07:08:04 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2071,6 +2071,7 @@ declare namespace $ {
         code: RegExp;
         'code-indent': RegExp;
         table: RegExp;
+        grid: RegExp;
         cut: RegExp;
         block: RegExp;
     }>;
--- web.d.ts
+++ web.d.ts
@@ -2047,6 +2047,7 @@ declare namespace $ {
         code: RegExp;
         'code-indent': RegExp;
         table: RegExp;
+        grid: RegExp;
         cut: RegExp;
         block: RegExp;
     }>;
