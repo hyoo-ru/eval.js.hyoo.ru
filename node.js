47d826d57359bdc8f8944624ebf173ff28081f09
nin-jin <nin-jin@users.noreply.github.com>
2023-05-18 17:06:30 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -293,6 +293,9 @@ var $;
         }
         fresh() { }
         complete() { }
+        get incompleted() {
+            return false;
+        }
         emit(quant = $mol_wire_cursor.stale) {
             for (let i = this.sub_from; i < this.data.length; i += 2) {
                 ;
@@ -546,6 +549,11 @@ var $;
             const limit = this.cursor < 0 ? this.sub_from : this.cursor;
             for (let cursor = this.pub_from; cursor < limit; cursor += 2) {
                 const pub = this.data[cursor];
+                if (pub?.incompleted)
+                    return;
+            }
+            for (let cursor = this.pub_from; cursor < limit; cursor += 2) {
+                const pub = this.data[cursor];
                 pub?.complete();
             }
         }
@@ -668,6 +676,9 @@ var $;
             if (this.cache instanceof Error)
                 return;
             return this.cache;
+        }
+        get incompleted() {
+            return $mol_promise_like(this.cache);
         }
         field() {
             return this.task.name + '()';
@@ -9491,7 +9502,13 @@ var $;
             spy_run() {
                 this.result([
                     ...this.result(),
-                    ...this.spy_queue.splice(0).map(([name, task]) => [name, ...[].concat($mol_try(() => task()))]),
+                    ...this.spy_queue.splice(0).map(([name, task]) => {
+                        try {
+                            return [name].concat(task());
+                        }
+                        catch (error) {
+                        }
+                    }).filter(Boolean),
                 ]);
             }
             spy(name, task) {
