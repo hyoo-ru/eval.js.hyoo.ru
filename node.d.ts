5cb56515e4121acbdde5f76e531c334776fbb2aa
nin-jin <nin-jin@users.noreply.github.com>
2025-01-02 07:13:20 +0000

    Deploying to gh-pages from @ hyoo-ru/eval.js.hyoo.ru@67f0d847339dd9fc1454e3de80483ccc91c9a731 🚀

--- node.d.ts
+++ node.d.ts
@@ -2044,7 +2044,7 @@ declare namespace $ {
 		uri( ): string
 		loading( ): string
 		decoding( ): string
-		cors( ): any
+		cors( ): string
 		natural_width( ): number
 		natural_height( ): number
 		load( next?: any ): any
