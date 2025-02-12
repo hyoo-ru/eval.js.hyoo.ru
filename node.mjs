8ddaa64aeac63fbbc6101a83d29efaf8b8da0afa
nin-jin <nin-jin@users.noreply.github.com>
2025-02-12 07:13:29 +0000

    Deploying to gh-pages from @ hyoo-ru/eval.js.hyoo.ru@67f0d847339dd9fc1454e3de80483ccc91c9a731 🚀

--- node.mjs
+++ node.mjs
@@ -7779,7 +7779,7 @@ var $;
             event_change(next) {
                 if (!next)
                     return;
-                const el = next.target;
+                const el = this.dom_node();
                 const from = el.selectionStart;
                 const to = el.selectionEnd;
                 try {
