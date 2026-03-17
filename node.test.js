3c71b2654014a9ad91acdeff4bcae8cd6f04a984
nin-jin <nin-jin@users.noreply.github.com>
2026-03-17 07:51:43 +0000

    Deploying to gh-pages from @ hyoo-ru/eval.js.hyoo.ru@67f0d847339dd9fc1454e3de80483ccc91c9a731 🚀

--- node.test.js
+++ node.test.js
@@ -5813,6 +5813,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
